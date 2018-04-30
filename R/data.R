8073cbfe02b1ba03b3a961051d5e13797256499d
Jaroslaw Piskorski <jaropis@zg.home.pl>
2018-04-30 11:43:00 +0200

    some changes to documentation

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' RR intervals time series with annotations (flags)
 #'
-#' @source own ECG
-#' @format Dataframe containing an
+#' @source my own ECG
+#' @format Dataframe containing an RR intervals time series with annotations.
 #' \describe{
 #' \item{RR}{RR intervals}
 #' \item{flags}{annotations for the RR intervals, 0 stands for a beat of sinus origin}
--- R/hrvhra.R
+++ R/hrvhra.R
@@ -11,18 +11,20 @@
 #' @import assertthat
 
 preparepp <- function(rr, annotations = c()) {
-
   # checking if RR vector the correct type and is long enough to proceed
   assert_that(is.vector(rr), is.numeric(rr), noNA(rr),
-              msg="the rr vector is either 1) not a vector, or 2) is not numeric or 3) has missing values")
-  assert_that(length(rr) > 3, msg="RR vector too short to form a Poincare plot")
+              msg = "the rr vector is either 1) not a vector, or 2) is not numeric or 3) has missing values")
+  assert_that(length(rr) > 3, msg = "RR vector too short to form a Poincare plot")
 
   # checking if annotations vector the coannotationsect type and is long enough to proceed
-  assert_that(is.vector(annotations), is.numeric(annotations), noNA(annotations),
-              msg="the annotations vector is either 1) not a vector, or 2) is not numeric or 3) has missing values")
-  assert_that(length(annotations) == length(rr), msg="annotations and RR vectors need to be of the same length")
+  assert_that(is.vector(annotations),
+              is.numeric(annotations),
+              noNA(annotations),
+              msg = "the annotations vector is either 1) not a vector, or 2) is not numeric or 3) has missing values")
+  assert_that(length(annotations) == length(rr), msg = "annotations and RR vectors need to be of the same length")
 
-  if (length(annotations) == 0) # assume all sinus beats if no annotations
+  if (length(annotations) == 0)
+    # assume all sinus beats if no annotations
     annotations = 0 * rr
 
   rr_i <- rr[1:(length(rr) - 1)]
@@ -39,7 +41,7 @@ preparepp <- function(rr, annotations = c()) {
   }
 
   # check if there are enough beats of sinus origin to proceed
-  assert_that(length(rr_i) > 2, msg="too many non-sinus beats to proceed")
+  assert_that(length(rr_i) > 2, msg = "too many non-sinus beats to proceed")
 
   return(cbind(rr_i, rr_ii))
 }
@@ -58,13 +60,20 @@ preparepp <- function(rr, annotations = c()) {
 #' drawpp(RR$RR, RR$flags)
 #' drawpp(RR$RR, RR$flags, vname="PP", col="gray", bg="blue")
 
-drawpp <- function(rr, annotations, vname="RR", ...) {
-
-  assert_that(is.character(vname), msg="vname needs to be a string")
+drawpp <- function(rr, annotations, vname = "RR", ...) {
+  assert_that(is.character(vname), msg = "vname needs to be a string")
   pp <- preparepp(rr, annotations)
   additional = list(...)
   # default appearance of the Poincare plot
-  list_of_parameters <- list(pp[, 2] ~ pp[, 1], xlab=parse(text = paste(vname, "[i]")), ylab=parse(text = paste(vname, "[i+1]")), pch=21, col="black", bg="orange")
+  list_of_parameters <-
+    list(
+      pp[, 2] ~ pp[, 1],
+      xlab = parse(text = paste(vname, "[i]")),
+      ylab = parse(text = paste(vname, "[i+1]")),
+      pch = 21,
+      col = "black",
+      bg = "orange"
+    )
 
   # append additional parameters - any error here will be dealt with plot
   for (parameter in names(additional)) {
@@ -82,7 +91,7 @@ drawpp <- function(rr, annotations, vname="RR", ...) {
 
 #' HRV descriptors
 #'
-#' This function returns the basic HRV descriptors related to the Poincare plot
+#' This function returns the basic HRV descriptors related to the Poincare plot.
 #'
 #' @inheritParams preparepp
 #' @return an 1 x 10 vector containing SDNN, SD1, SD2, SD1I, SDNNd, SDNNa, SD1d, SD1a, SD2d, SD2a  descriptors
@@ -102,7 +111,7 @@ hrvhra <- function(rr, annotations) {
   # start with classic HRV parameters
 
   n <- length(rr_i)
-  correct <- (n-1) / n # correction to use biased variance
+  correct <- (n - 1) / n # correction to use biased variance
 
   sd1 <- sqrt(var((rr_i - rr_ii) / sqrt(2)) * correct)
   sd2 <- sqrt(var((rr_i + rr_ii) / sqrt(2)) * correct)
@@ -142,7 +151,8 @@ hrvhra <- function(rr, annotations) {
   sdnna <- sqrt(1 / 2 * (sd1a ^ 2 + sd2a ^ 2))
   sdnnd <- sqrt(1 / 2 * (sd1d ^ 2 + sd2d ^ 2))
   results_hra <- c(sdnnd, sdnna, sd1d, sd1a, sd2d, sd2a)
-  names(results_hra) <- c("SDNNd", "SDNNa", "SD1d", "SD1a", "SD2d", "SD2a")
+  names(results_hra) <-
+    c("SDNNd", "SDNNa", "SD1d", "SD1a", "SD2d", "SD2a")
   results <- c(results_hrv, results_hra)
 
   return(results)
@@ -160,17 +170,25 @@ hrvhra <- function(rr, annotations) {
 #' @examples
 #' describerr(RR$flags)
 
-describerr <- function(annotations){
-  assert_that(is.vector(annotations), is.numeric(annotations), noNA(annotations),
-              msg="the annotations vector is either 1) not a vector, or 2) is not numeric or 3) has missing values")
+describerr <- function(annotations) {
+  assert_that(is.vector(annotations),
+              is.numeric(annotations),
+              noNA(annotations),
+              msg = "the annotations vector is either 1) not a vector, or 2) is not numeric or 3) has missing values")
   all_beats <- length(annotations)
   sinus_beats <- sum(annotations == 0)
   ventricular_beats <- sum(annotations == 1)
   supraventricular_beats <- sum(annotations == 2)
   artifacts <- sum(annotations == 3)
   unidentified_beats <- sum(!(annotations %in% c(0, 1, 2, 3)))
-  statistics <- c(all_beats, sinus_beats, ventricular_beats,
-                  supraventricular_beats, artifacts, unidentified_beats)
+  statistics <- c(
+    all_beats,
+    sinus_beats,
+    ventricular_beats,
+    supraventricular_beats,
+    artifacts,
+    unidentified_beats
+  )
   names(statistics) <- c("all", "N", "V", "S", "X", "U")
   return(statistics)
 }
