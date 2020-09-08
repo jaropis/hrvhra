d11a7a6fdda27a5abc83d54d3d6018b35a9758b0
Jarosław Piskorski <jaropis@zg.home.pl>
2020-09-08 21:56:21 +0200

    spectral analysis ready

--- R/spectral (jaropis-Extensa-2540's conflicted copy 2020-09-08).R
+++ R/spectral (jaropis-Extensa-2540's conflicted copy 2020-09-08).R
@@ -1,11 +1,3 @@
-#' Frequency bands 24 h
-#' @export
-frequency_bands_24 <- list(ULF = c(0, 0.003), VLF = c(0.003, .04), LF = c(0.04, 0.15), HF = c(0.15, 0.40))
-
-#' Frequency bands < 24 h
-#' @export
-frequency_bands <- list(VLF = c(0.00, .04), LF = c(0.04, 0.15), HF = c(0.15, 0.40))
-
 #' Function calculating Lomb-Scargle periodogram and returning power within the pre-defined bands
 #' power is normalized so that the total power is equal to the variance of the recording
 #' @param RR RR object
@@ -26,23 +18,14 @@ lomb_spectrum <- function(RR) {
 
 #' Function to calculate power in specific bands
 #' @param spectrum list with power and frequency
-#' @param bands list in the form list(LF=c(0.00, .04)) etc.
+#' @param bands list in the form list(LF=c(0.01, 0.1)) etc.
 #' @return list with bands names as names and power in bands as the values
-calculate_frequency_bands <- function(spectrum, bands = frequency_bands) {
+#' @export
+calculate_frequency_bands <- function(spectrum, bands) {
   power_in_bands <- lapply(bands, function(band) {
-    sum(spectrum$power[spectrum$frequency >= band[1] & spectrum$frequency <= band[2]])
+    sum(spectrum$power[spectrum$frequency >= band[1] && spectrum$frequency <= band[2]])
   })
   names(power_in_bands) <- names(bands)
-  power_in_bands["TP"] <- sum(unlist(power_in_bands))
   return(power_in_bands)
 }
 
-#' Master function getting the results
-#' @param RR RR object
-#' @param bands frequency bands
-#' @return list with power in bands
-#' @export
-calculate_RR_spectrum <- function(RR, bands = frequency_bands) {
-  lomb_spectrum(RR) %>%
-    calculate_frequency_bands(bands)
-}
