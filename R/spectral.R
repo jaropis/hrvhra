a80c8c4b6a8a9a25a89914a193759511ca1e73ec
Jaroslaw Piskorski <jaropis@zg.home.pl>
2020-09-08 16:26:46 +0200

    calculating power in bands with Lomb-Scargle algorighm

--- R/spectral.R
+++ R/spectral.R
@@ -1,5 +1,6 @@
 #' Function calculating Lomb-Scargle periodogram and returning power within the pre-defined bands
 #' @param RR RR object
+#' @return list with power and frequency
 lomb_spectrum <- function(RR) {
   x_ts <- data.frame(sampling = cumsum(RR$RR) / 1000, # to get the results in Hz on x and ms^2 on y
                      samples = RR$RR)
@@ -9,5 +10,21 @@ lomb_spectrum <- function(RR) {
   spectrum <- lomb::lsp(x_ts,
                         type = "frequency",
                         plot = FALSE)
-  # total power will be sum(spectrum$power) * diff(spectrum$scanned)[1]  * 2 * var(RR$RR)
+  # normalizing power spectrum so that we have correspondence between time domain and frequency domain
+  power <- spectrum$power * 1/length(spectrum$scanned)  * var(RR$RR)
+  return(list(power = power, frequency = spectrum$scanned))
 }
+
+#' Function to calculate power in specific bands
+#' @param spectrum list with power and frequency
+#' @param bands list in the form list(LF=c(0.01, 0.1)) etc.
+#' @return list with bands names as names and power in bands as the values
+#' @export
+calculate_frequency_bands <- function(spectrum, bands) {
+  power_in_bands <- lapply(bands, function(band) {
+    sum(spectrum$power[spectrum$frequency >= band[1] && spectrum$frequency <= band[2]])
+  })
+  names(power_in_bands) <- names(bands)
+  return(power_in_bands)
+}
+
